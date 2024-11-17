5fc8c7e7fa8b75eead63dc0fba7ef096425e77f7
Kishan <kishanmishra267@gmail.com>
2024-11-18 02:10:01 +0530

    Started Decimal to binary converter App

---
+++ DecimalToBinaryConverter/script.js
@@ -0,0 +1,52 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const countdown = (number) => {
+  console.log(number);
+
+  if (number === 0) {
+    return;
+  } else {
+    countdown(number - 1);
+  }
+};
+
+countdown(3);
+
+const decimalToBinary = (input) => {
+  let binary = "";
+
+  if (input === 0) {
+    binary = "0";
+  }
+
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
+
+  result.innerText = binary;
+};
+
+const checkUserInput = () => {
+  if (
+    !numberInput.value ||
+    isNaN(parseInt(numberInput.value)) ||
+    parseInt(numberInput.value) < 0
+  ) {
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
